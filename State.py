b74f779f1e3a5bf8ecfc8f11f27e90ca7951659e
Andrew Rocchi <andrewroc30@gmail.com>
2019-04-19 03:25:39 -0400

    Added Trend Feature

--- State.py
+++ State.py
@@ -37,16 +37,16 @@ def getLastNPrices(numDays, row, pricesData):
 
 # TODO change to include stuff other than just closing prices
 class State:
-    def __init__(State, account, index, numStocks, lastNClosingPrices):
+    def __init__(State, account, index, numStocks, lastNClosingPrices, trend):
         # for normalization:
         #max_x = max(account,index,numStocks,max(lastNClosingPrices))
         #min_x = min(account,index,numStocks,max(lastNClosingPrices))
         #(2 * (account - min_x) / (max_x - min_x)) - 1
-
         State.account = account
         State.index = index
         State.numStocks = numStocks
         State.lastNClosingPrices = lastNClosingPrices
+        State.trend = trend
 
     # Returns array of last N prices starting at (including) index row from array of prices v
     # if numDays is 0 then it just gives today's price
@@ -57,20 +57,19 @@ class State:
 
     # returns the next state given the action and the closing price of the next day
     # TODO change to include stuff other than just closing prices
-    # TODO change to include buying and selling other quantities
-    # TODO CHECK IF ACTION IS DOABLE IN OUTER LOOP
     def getNextState(self, action, nextPrice):
         newClosingPrices = self.lastNClosingPrices[1:]
         newClosingPrices.append(nextPrice)
         newAccount = self.account
         newNumStocks = self.numStocks
+        trend = (newClosingPrices[0] - newClosingPrices[-1]) / len(newClosingPrices)  # trend is slope between first and last day
         if action.action == act.Actions.buy:
             newAccount -= self.lastNClosingPrices[-1] * action.amount
             newNumStocks += action.amount
         elif action.action == act.Actions.sell:
             newAccount += self.lastNClosingPrices[-1] * action.amount
             newNumStocks -= action.amount
-        return State(newAccount, self.index+1, newNumStocks, newClosingPrices)
+        return State(newAccount, self.index + 1, newNumStocks, newClosingPrices, trend)
 
     # the reward is the increase in the value of your portfolio
     # returns reward received if the given action is carried out on this state
@@ -84,7 +83,7 @@ class State:
 
     def calcQvalue(self, weights):
         return (weights.accountWeight * self.account) + (weights.numStocksWeight * self.numStocks) \
-                + np.dot(self.lastNClosingPrices, weights.pricesWeights)
+                + np.dot(self.lastNClosingPrices, weights.pricesWeights) + (weights.trendWeight * self.trend)
 
     # returns the best action and maxQ value for this state (Q value if that action was applied to it),takes in list
     # possible actions given current weights
@@ -110,28 +109,29 @@ class State:
         newAccountWeight = oldWeights.accountWeight + (alpha * difference * self.account)
         newNumStocksWeight = oldWeights.numStocksWeight + (alpha * difference * self.numStocks)
         newPricesWeights = [sum(x) for x in zip(oldWeights.pricesWeights, [alpha * difference * price for price in self.lastNClosingPrices])]
-        max_x = max(newAccountWeight, newNumStocksWeight, max(newPricesWeights))
-        min_x = min(newAccountWeight, newNumStocksWeight, min(newPricesWeights))
+        newTrendWeight = oldWeights.trendWeight + (alpha * difference * self.trend)
+        max_x = max(newAccountWeight, newNumStocksWeight, max(newPricesWeights), newTrendWeight)
+        min_x = min(newAccountWeight, newNumStocksWeight, min(newPricesWeights), newTrendWeight)
         range = max_x - min_x
         if range != 0:
             norm_newpricesweight = [(2 * (priceweight - min_x) / range) - 1 for priceweight in newPricesWeights]
-            return w.Weights((2 * (newAccountWeight - min_x) / range) - 1, (2 * (newNumStocksWeight - min_x)/range) - 1, norm_newpricesweight)
+            return w.Weights((2 * (newAccountWeight - min_x) / range) - 1, (2 * (newNumStocksWeight - min_x)/range) - 1, norm_newpricesweight, (2 * (newTrendWeight - min_x)/range) - 1)
         elif min_x == 0 and max_x == 0:
             norm_newpricesweight = [0 for priceweight in newPricesWeights]
-            return w.Weights(0, 0, norm_newpricesweight)
+            return w.Weights(0, 0, norm_newpricesweight, 0)
         norm_newpricesweight = [1 for priceweight in newPricesWeights]
-        return w.Weights(1, 1, norm_newpricesweight)
+        return w.Weights(1, 1, norm_newpricesweight, 1)
 
     # returns action based on epsilon
     def EpsilonPolicy(self, epsilon, actions, weights, nextPrice):
-        max, bestA = self.maxQAndAction(weights, actions, nextPrice)
         choices = ['best', 'random']
         choice = np.random.choice(choices, p=[(1 - epsilon), epsilon])
         if choice == 'best':
+            max, bestA = self.maxQAndAction(weights, actions, nextPrice)
             return bestA
         else:
             withoutBest = actions[:]
-            withoutBest.remove(bestA)
+            # withoutBest.remove(bestA)
             return np.random.choice(withoutBest)
 
     # Chooses the best action to take from a given state
@@ -160,22 +160,6 @@ def plotChoices(v, choices):
     plt.show()
 
 
-def plotChoices_eval(v_eval, choices):
-    plt.plot(v_eval)
-    count = 0
-    for i in v_eval:
-        if count >= len(choices):
-            break
-        elif choices[count].action == act.Actions.buy:
-            plt.scatter(count, i, c='green')
-        elif choices[count].action == act.Actions.sell:
-            plt.scatter(count, i, c='red')
-        # elif choices[count].action == act.Actions.hold:
-        #     plt.scatter(count, i, c='blue')
-        count += 1
-    plt.show()
-
-
 def plotBalances(states):
     x = []
     y = []
@@ -191,14 +175,61 @@ def plotBalances(states):
     plt.show()
 
 
+# def plotWeights(weights, episodes):
+#     all_weights = []
+#     x = []
+#     # for w in weights:
+#     #     tempWeights = []
+#     #     tempWeights.append(w.accountWeight)
+#     #     tempWeights.append(w.numStocksWeight)
+#     #     for p in w.pricesWeights:
+#     #         tempWeights.append(p)
+#     #     tempWeights.append(w.trendWeight)
+#     #     all_weights.append(tempWeights)
+#     accountWeights = []
+#     numStocksWeights = []
+#     pricesWeightsWeights = []
+#     trendWeights = []
+#     for w in weights:
+#         accountWeights.append(w.accountWeight)
+#         numStocksWeights.append(w.numStocksWeight)
+#         pricesWeightsWeights.append(w.pricesWeights)
+#         trendWeights.append(w.trendWeight)
+#     all_weights.append(accountWeights)
+#     all_weights.append(numStocksWeights)
+#     for p in pricesWeightsWeights:
+#         all_weights.append(p)
+#     all_weights.append(trendWeights)
+#     # print(len(all_weights))
+#     # print(len(all_weights[0]))
+#     for i in range(episodes):
+#         x.append(i)
+#     # print(len(x))
+#     for w in all_weights:
+#         print(len(w))
+#         print(w)
+#     count = 0
+#     for w in all_weights:
+#         plt.plot(x, w, label=count)
+#         count += 1
+#     plt.legend(loc="lower left")
+#     plt.xlabel('episode')
+#     plt.ylabel('weighting')
+#     plt.title('Weights')
+#     plt.show()
+
+
 def QLearn(episodes, epsilon, gamma, alpha, startingAccount, n, actions):
-    weights = w.Weights(0, 0, [0 for i in range(n)])
+    np.random.seed(50)
+    weights = w.Weights(0, 0, [0 for i in range(n)], 0)
     epsilon_change = epsilon / episodes
     choices = []
     states = []
+    all_weights = []
     for i in range(episodes):
         lastNPrices = getLastNPrices(n, 0, v)
-        state = State(startingAccount, 0, 0, lastNPrices)
+        trend = (lastNPrices[0] - lastNPrices[-1]) / len(lastNPrices)
+        state = State(startingAccount, 0, 0, lastNPrices, trend)
         for j, price in enumerate(v):
             if j < len(v) - 2:
                 action = state.chooseAction(epsilon, actions, weights, v, state, j)
@@ -213,14 +244,17 @@ def QLearn(episodes, epsilon, gamma, alpha, startingAccount, n, actions):
         if i % 10 == 0:
             print("Episode: " + str(i) + ", Account: " + str(state.account) + ", numStocks: " + str(state.numStocks))
         states.append(state)
+        all_weights.append(weights)
     plotChoices(v, choices)
     plotBalances(states)
+    # plotWeights(all_weights, episodes)
     return weights
 
 
 def evaluate(weights, startingAmount, n, actions):
     lastNPrices = getLastNPrices(n, 0, v_eval)
-    state = State(startingAmount, 0, 0, lastNPrices)
+    trend = (lastNPrices[0] - lastNPrices[-1]) / len(lastNPrices)
+    state = State(startingAmount, 0, 0, lastNPrices, trend)
     choices = []
     cantSell = [x for x in actions if x.action != act.Actions.sell]
     for j, price in enumerate(v_eval):
@@ -233,22 +267,22 @@ def evaluate(weights, startingAmount, n, actions):
             choices.append(action)
             state = state.getNextState(action, nextPrice)
     print("account_eval:" + str(state.account))
-    plotChoices_eval(v_eval, choices)
+    plotChoices(v_eval, choices)
 
 
 buy = act.Action(act.Actions.buy, 1)
 sell = act.Action(act.Actions.sell, 1)
 hold = act.Action(act.Actions.hold, 1)
 actions = [buy, sell, hold]
-for n in range(2, 3):
-    actions.append(act.Action(act.Actions.buy, n))
-    actions.append(act.Action(act.Actions.sell, n))
-    actions.append(act.Action(act.Actions.hold, n))
-weights = w.Weights(0.5, -1, [1, 3, 4, 6, 7])
-lastNPrices = getLastNPrices(5, 5, v)
-currentState = State(10000, 5, 0, lastNPrices)
+# for n in range(2, 3):
+#     actions.append(act.Action(act.Actions.buy, n))
+#     actions.append(act.Action(act.Actions.sell, n))
+#     actions.append(act.Action(act.Actions.hold, n))
+# weights = w.Weights(0.5, -1, [1, 3, 4, 6, 7])
+# lastNPrices = getLastNPrices(5, 5, v)
+# currentState = State(10000, 5, 0, lastNPrices)
 
-evaluate(QLearn(100, 0.6, 0.8, 0.001, 10000, 20, actions), 10000, 20, actions)
+evaluate(QLearn(7, 0.9, 0.1, 0.001, 10000, 5, actions), 10000, 5, actions)
 
 
 # print(currentState)
--- Weights.py
+++ Weights.py
@@ -1,8 +1,9 @@
 class Weights:
-    def __init__(Weights, accountWeight, numStocksWeight, pricesWeights):
+    def __init__(Weights, accountWeight, numStocksWeight, pricesWeights, trendWeight):
         Weights.accountWeight = accountWeight
         Weights.numStocksWeight = numStocksWeight
         Weights.pricesWeights = pricesWeights
+        Weights.trendWeight = trendWeight
 
     def __str__(self):
         return "accountWeight:" + str(self.accountWeight) + "\n" + "numstocksWeight:" + str(self.numStocksWeight) + \
